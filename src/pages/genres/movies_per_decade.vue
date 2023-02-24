ba718d7b24de57dd4ea564c787c0af5826d605cf
Ced <79917173+Revkxd@users.noreply.github.com>
2023-02-24 16:53:22 +0800

    Started including Genres tab
    TODO make queries for genre based

---
+++ src/pages/genres/movies_per_decade.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="p-8">
+    <section class="grid gap-1 p-4">
+      <h1 class="text-2xl font-bold tracking-tight">Movies per decade</h1>
+      <p>view the movies in genres through the decades</p>
+    </section>
+
+    <TabGroup>
+      <div class="mb-2 flex items-center justify-end gap-4 text-sm">
+        <SelectorActors v-model="actor" class="z-20 mr-auto" />
+
+        <span v-if="fetchTime" class="text-gray-400">Fetch Time: {{ fetchTime }} ms</span>
+
+        <TabOptions />
+      </div>
+
+      <TabResults :result="result">
+        <div v-if="!loading && data.decades.length">
+          <client-only>
+            <apexchart type="bar" height="350" :options="chart.options" :series="chart.series"></apexchart>
+          </client-only>
+        </div>
+      </TabResults>
+    </TabGroup>
+
+    <p class="text-sm text-red-600">{{ error }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { TabGroup } from "@headlessui/vue";
+import { Ref } from "vue";
+
+const { runQuery, result, fetchTime, loading, error } = useQuery();
+
+const data = reactive<Object>({
+  count_movies: [],
+  decades: [],
+});
+
+const chart = reactive<Object>({
+  series: [],
+  options: {},
+});
+
+const actor: Ref<Object> = ref({});
+
+watch(actor, () =>
+  // TODO create query that searches by genre
+  runQuery(`
+  SELECT	decade, 
+          COUNT(movie_id) AS count_movies
+  FROM (
+    SELECT  roles.actor_id,
+            roles.movie_id,
+            FLOOR(movies.year/10)*10 AS decade
+    FROM    roles
+    LEFT JOIN movies ON movies.id = roles.movie_id
+    WHERE   roles.actor_id = ${actor.value.id}
+  ) t
+  LEFT JOIN actors ON actors.id = t.actor_id
+  GROUP BY  actor_id, decade
+  ORDER BY  decade
+`)
+);
+
+// TODO adjust this
+watch(result, () => {
+  data.count_movies = result.value.map((row: Object) => row.count_movies);
+  data.decades = result.value.map((row: Object) => row.decade);
+
+  chart.series = [
+    {
+      name: "Number of movies",
+      data: data.count_movies,
+    },
+  ];
+
+  chart.options = {
+    plotOptions: {
+      bar: {
+        borderRadius: 4,
+        horizontal: false,
+      },
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    xaxis: {
+      categories: data.decades,
+    },
+  };
+});
+</script>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -7,12 +7,10 @@
     </p>
   </section>
 
-  <section class="grid w-full gap-8 px-12 text-justify leading-5 text-gray-900">
+  <section class="flex w-full flex-row gap-8 px-12 text-justify leading-5 text-gray-900">
     <div class="grid max-w-sm gap-4 rounded-lg border border-gray-200 bg-white p-6 shadow">
       <h3 class="text-2xl font-bold tracking-tight">Actors</h3>
-      <p class="text-justify">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sem eu vitae laoreet odio augue euismod neque.
-      </p>
+      <p class="text-justify">List of movies featuring actors based on a time frame.</p>
 
       <ul class="grid max-w-md gap-1 space-y-1">
         <li>
@@ -26,6 +24,26 @@
             Movies per decade
           </NuxtLink>
           - view the count of movies an actor has starred in through the decades
+        </li>
+      </ul>
+    </div>
+
+    <div class="grid max-w-sm gap-4 rounded-lg border border-gray-200 bg-white p-6 shadow">
+      <h3 class="text-2xl font-bold tracking-tight">Genres</h3>
+      <p class="text-justify">List of movies according to genre based on a time frame.</p>
+
+      <ul class="grid max-w-md gap-1 space-y-1">
+        <li>
+          <NuxtLink to="genres/movies_per_year" class="font-medium text-blue-600 hover:underline">
+            Movies per year
+          </NuxtLink>
+          - explore the annual count of movies in a genre
+        </li>
+        <li>
+          <NuxtLink to="genres/movies_per_decade" class="font-medium text-blue-600 hover:underline">
+            Movies per decade
+          </NuxtLink>
+          - explore movies in a genre through the decades
         </li>
       </ul>
     </div>
