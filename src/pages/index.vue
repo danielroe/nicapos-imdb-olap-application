4451ffea2ac4d2b95960fdfdc1941aa56c1caafa
Ced <79917173+Revkxd@users.noreply.github.com>
2023-02-24 18:31:19 +0800

    styling changes

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -11,8 +11,8 @@
   </section>
 
   <section class="flex w-full flex-row gap-8 px-12 text-justify leading-5 text-gray-900">
-    <div class="grid max-w-sm gap-4 rounded-lg border border-gray-200 bg-white p-6 shadow">
-      <h3 class="text-2xl font-bold tracking-tight">Actors</h3>
+    <div class="grid w-96 max-w-sm gap-4 rounded-lg border border-gray-200 bg-white p-6 shadow">
+      <h3 class="text-center text-2xl font-bold tracking-tight">Actors</h3>
       <p class="text-justify">List of movies featuring actors based on a time frame.</p>
 
       <ul class="grid max-w-md gap-1 space-y-1">
@@ -31,8 +31,8 @@
       </ul>
     </div>
 
-    <div class="grid max-w-sm gap-4 rounded-lg border border-gray-200 bg-white p-6 shadow">
-      <h3 class="text-2xl font-bold tracking-tight">Genres</h3>
+    <div class="grid w-96 max-w-sm gap-4 rounded-lg border border-gray-200 bg-white p-6 shadow">
+      <h3 class="text-center text-2xl font-bold tracking-tight">Genres</h3>
       <p class="text-justify">List of movies according to genre based on a time frame.</p>
 
       <ul class="grid max-w-md gap-1 space-y-1">
