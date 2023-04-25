ad1c13e0396d39335827d770876029de4ccab8c0
nicapos <annikaposadas22@gmail.com>
2023-04-25 17:17:46 +0800

    remove demo page

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -4,10 +4,6 @@
       <nav class="flex w-full items-center gap-4 bg-slate-800 py-2 px-4 text-slate-100">
         <Icon name="emojione:bar-chart" class="h-5 w-5" />
         <NuxtLink to="/" class="font-bold">OLAP App Title</NuxtLink>
-
-        <button class="ml-auto rounded bg-slate-700 p-2 text-xs">
-          <NuxtLink to="/demo">SQL Query Area</NuxtLink>
-        </button>
       </nav>
       <slot />
     </div>
